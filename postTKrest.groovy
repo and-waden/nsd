04775d69e5dcf83ea1f80710a8ae0be461ad47a1
and-waden <a.vodenikov@gmail.com>
2020-02-12 15:02:29 +0300

    Create postTKrest.groovy

---
+++ postTKrest.groovy
@@ -0,0 +1,10 @@
+//rest post создать коммент к ТК
+https://coffeemania-test.itsm365.com/sd/services/rest/create/comment/?accessKey=412103b4-9baf-454d-b14f-a2ebc9012337
+{
+"source":"techcart$5678801",
+"text":"Комментарий для техкарты через интеграцию"
+}
+//rest post создать ТК с мин параметрами
+https://coffeemania-test.itsm365.com/sd/services/rest/create-m2m/techcart/?accessKey=412103b4-9baf-454d-b14f-a2ebc9012337
+{"metaClass":"techcart$techcart",
+"title":"Боул с синим через int"}
